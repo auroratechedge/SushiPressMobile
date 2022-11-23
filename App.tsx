035f6406faef4dd47f44f82a8b23e248f28ed117
Aurora Cucciniello <117350688+auroratechedge@users.noreply.github.com>
2022-11-23 16:16:40 +0100

    minor fix

--- App.tsx
+++ App.tsx
@@ -8,41 +8,17 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  Image,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import React from 'react';
 import Home from './src/screens/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Menu from './src/screens/Menu';
-import {createDrawerNavigator, DrawerItemList} from '@react-navigation/drawer';
 import 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import {store} from './src/store/storeConfiguration';
 import Cart from './src/screens/Cart';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   const Stack = createNativeStackNavigator();
 
@@ -61,24 +37,5 @@ const App = () => {
     </Provider>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
